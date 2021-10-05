d14fe8b128c144649e87aa1f4eeae81c673cfefc
Olegio <57033651+ogorski@users.noreply.github.com>
2021-10-05 11:18:02 +0300

    Delete react-projects directory

--- react-projects/react-native-projects/project_one/src/components/Main/Feed/Feed.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import s from './Feed.module.scss'
-
-export const Feed = () => {
-    return (
-        <div className={s.feed}></div>
-    )
-}
--- react-projects/react-native-projects/template/src/components/Main/Main.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import styles from './styles'
-import {Grid, Typography, Divider} from '@material-ui/core'
-import Markdown from './'
